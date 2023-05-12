a486c033ba3f1abf75dccefee090b8cc4e0735e5
Yuchanher <120745695+Yuchanher@users.noreply.github.com>
2023-05-12 19:27:18 -0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,39 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Motorola',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+  },
+  {
+    id: 2,
+    name: 'Iphone',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+  },
+  {
+    id: 3,
+    name: 'Xiaome',
+    price: 299,
+    description: '',
+  },
+  {
+    id: 4,
+    name: 'Sansung Galax',
+    price: 99,
+    description: '',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
